30e43af224f205fe6f9804ac5a0cc0465368e3b3
lashasaata <lashasaata@gmail.com>
2024-05-28 04:48:39 -0700

    style: add mobile styles for Navigate component

--- src/components/Navigate.tsx
+++ src/components/Navigate.tsx
@@ -1,13 +1,17 @@
 function Navigate() {
   return (
-    <header>
-      <div>
+    <header className="flex justify-between items-center px-6 pt-[19px] pb-6">
+      <div className="flex gap-4 items-center">
         <img src="/images/icon-menu.svg" alt="menu" />
         <img src="/images/logo.svg" alt="logo" />
       </div>
-      <div>
+      <div className="flex items-center gap-[22px]">
         <img src="/images/icon-cart.svg" alt="cart" />
-        <img src="/images/image-avatar.png" alt="avatar" />
+        <img
+          src="/images/image-avatar.png"
+          alt="avatar"
+          className="w-6 h-6 rounded-full"
+        />
       </div>
     </header>
   );
