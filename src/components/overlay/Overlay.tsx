46c958af86afdab3101c8f7e5f5195237233b5b3
lashasaata <lashasaata@gmail.com>
2024-06-02 21:25:06 -0700

    style: add styles for overlay component

--- src/components/overlay/Overlay.tsx
+++ src/components/overlay/Overlay.tsx
@@ -6,22 +6,30 @@ function Overlay({
   const closeOverlay = () => {
     setOverlay(false);
   };
+  //
+  const routSt = {
+    fontSize: "18px",
+    lineHeight: "28px",
+    color: "#1d2026",
+    fontWeight: "700",
+  };
+
   return (
     <div className="absolute">
-      <div className="w-full h-full fixed top-0 left-0 z-10 bg-overlay">
-        <div className="w-[250px] h-full bg-[#fff] z-10">
+      <div className="w-screen h-screen fixed top-0 left-0 z-10 bg-overlay">
+        <div className="w-[250px] h-screen bg-[#fff] z-10">
           <div className="flex flex-col items-start gap-[54px] pt-[25px] pl-[25px]">
             <img
               onClick={closeOverlay}
               src="/images/icon-close.svg"
               alt="close"
             />
-            <nav>
-              <h2>Collections</h2>
-              <h2>Men</h2>
-              <h2>Women</h2>
-              <h2>About</h2>
-              <h2>Contact</h2>
+            <nav className="flex flex-col gap-5">
+              <h2 style={routSt}>Collections</h2>
+              <h2 style={routSt}>Men</h2>
+              <h2 style={routSt}>Women</h2>
+              <h2 style={routSt}>About</h2>
+              <h2 style={routSt}>Contact</h2>
             </nav>
           </div>
         </div>
