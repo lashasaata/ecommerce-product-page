bf41ceef4cda77b2da56e4cbe42307a0f684c915
lashasaata <lashasaata@gmail.com>
2024-06-02 20:46:00 -0700

    add elements in menu component

--- src/components/overlay/Overlay.tsx
+++ src/components/overlay/Overlay.tsx
@@ -10,13 +10,19 @@ function Overlay({
     <div className="absolute">
       <div className="w-full h-full fixed top-0 left-0 z-10 bg-overlay">
         <div className="w-[250px] h-full bg-[#fff] z-10">
-          <div>
+          <div className="flex flex-col items-start gap-[54px] pt-[25px] pl-[25px]">
             <img
               onClick={closeOverlay}
               src="/images/icon-close.svg"
               alt="close"
             />
-            <h1>xd</h1>
+            <nav>
+              <h2>Collections</h2>
+              <h2>Men</h2>
+              <h2>Women</h2>
+              <h2>About</h2>
+              <h2>Contact</h2>
+            </nav>
           </div>
         </div>
       </div>
