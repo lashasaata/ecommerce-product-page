02a1b360695f69cd604d8eae649248011df73b9e
lashasaata <lashasaata@gmail.com>
2024-07-20 04:17:40 -0700

    style: add close function on close img at overlay and style it

--- src/components/Product.tsx
+++ src/components/Product.tsx
@@ -83,7 +83,8 @@ function Product() {
 
   //
   const [useOverlay, setOverlay] = useState(false);
-  console.log(useOverlay);
+  const [useHover, setHover] = useState(false);
+
   return (
     <main
       onClick={() => context.setCartList(false)}
@@ -140,11 +141,14 @@ function Product() {
                           width="14"
                           height="15"
                           xmlns="http://www.w3.org/2000/svg"
-                          className="self-end mb-6"
+                          className="self-end mb-6 hover:cursor-pointer"
+                          onMouseEnter={() => setHover(true)}
+                          onMouseLeave={() => setHover(false)}
+                          onClick={() => setOverlay(false)}
                         >
                           <path
                             d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-                            fill="#fff"
+                            fill={useHover ? "#ff7e1b" : "#fff"}
                             fillRule="evenodd"
                           />
                         </svg>
